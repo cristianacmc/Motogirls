436d207bf8adcdf49d3425a331fae3cb29ac0983
cristianacmc <cristianacmc@gmail.com>
2015-10-12 21:50:18 -0300

    formulario edicao

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import Postform
+from django.shortcuts import redirect
+
 
 
 def post_list(request):
@@ -12,3 +15,25 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = Postform(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = Postform
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = Postform(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            post.save()
+            return redirect('blog/post_edit.html', {'form': form})
+    else:
+        form = Postform(instance=post)
+    return render(request,'blog/post_edit.html', {'form': form})
